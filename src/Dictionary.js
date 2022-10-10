544f7cda93ada7045b8b5835249df33a3ca676f4
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-10-10 14:49:08 +0900

    Added alert to search form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Dictionary.css";
 
 export default function Dictionary(){
+    let [KeyWord, setKeyWord] = useState(" ");
+
+function handleSubmit (event){
+    event.preventDefault();
+    alert(`looking for ${KeyWord}`);
+}
+
+function updateAlert (event){
+ setKeyWord(event.target.value);
+
+}
     return(
-        <div className="Dictionary">
+ <div className="Dictionary">
 <h1>Dictionary</h1>
-<form>
-<input type="search"/>
+<form onSubmit={handleSubmit}>
+<input type="search" onChange={updateAlert}/>
 <input type= "submit" value="search"/>
 </form>
 </div>
