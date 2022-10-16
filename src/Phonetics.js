a84e7fa8c58fd3af525e3097cf10f317e2f6e7d8
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-10-17 07:55:25 +0900

    Updated Phonetics.js with link to audio file

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,6 +3,8 @@ import React from "react";
 export default function Phonetics (props){
 return(
         <div className="phonetics">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer"> Listen </a>
+            <br />
             {props.phonetic.text}
         </div>
     );
