2e51c2cf8787455af6270ff8c9608aa1e58264cb
Tabby Blades <103293046+tabbyblades@users.noreply.github.com>
2022-10-19 07:29:58 +0900

    Updated search function and created a default KeyWord

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,11 +4,18 @@ import axios from "axios";
 import Results from "./Results";
 
 export default function Dictionary(){
-    let [KeyWord, setKeyWord] = useState(" ");
+    let [KeyWord, setKeyWord] = useState("smile");
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
 function handleSubmit (event){
     event.preventDefault();
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${KeyWord}`;
+axios.get(apiUrl).then(handleResponse);
+search();
+}
+
+function search(){
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${KeyWord}`;
 axios.get(apiUrl).then(handleResponse);
 }
@@ -22,17 +29,27 @@ function handleResponse(response){
     setResults(response.data[0]);
 }
 
+function load(){
+    setLoaded(true);
+    search();
+}
 
+if(loaded){
     return(
- <div className="Dictionary">
-<h1>Dictionary</h1>
-<form onSubmit={handleSubmit}>
-<input type="search" onChange={updateKeyWord} className="searchForm"/>
-<input type= "submit" value="search" className="submitButton"/>
-</form>
-<Results results={results}/>
-<p className="text-center mt-3">Coded by Tabby Blades and open-sourced on <a href="https://github.com/tabbyblades/dictionary-app-react" target="_blank" rel="noopener noreferrer">
-    Github</a></p>
-</div>
-    );
+        <div className="Dictionary">
+       <h1>Dictionary</h1>
+       <form onSubmit={handleSubmit}>
+       <input type="search" onChange={updateKeyWord} className="searchForm"/>
+       <input type= "submit" value="search" className="submitButton"/>
+       </form>
+       <Results results={results}/>
+       <p className="text-center mt-3">Coded by Tabby Blades and open-sourced on <a href="https://github.com/tabbyblades/dictionary-app-react" target="_blank" rel="noopener noreferrer">
+           Github</a></p>
+       </div>
+           );
+} else{
+    load();
+    return "loading"
+}
+    
 }
